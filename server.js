45fa74c3748545c583b7c66524d487f03bd66208
Achiuu13 <achiu@umass.edu>
2026-07-13 01:32:21 +0000

    initial Express server with two routes

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, web!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('I will be learning Web Dev.');
+});
+
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name
+  res.send(`Hello, ${name}!`)
+});
+
+app.get('/repeat/:word', (req, res) => {
+  const word = req.params.word
+  res.send(`${word} ${word} ${word}`)
+});
+
+app.get('/count', (req, res) => {
+  const from = req.query.from || 1
+  const to = req.query.from || 10
+
+  res.send(`Counting from ${from} to ${to}.`)
+});
+
+app.get('/api/info', (req, res) => {
+  res.json({
+    course: "COMPSCI 326",
+    team: 3
+  })
+});
+
+app.get('/api/error', (req, res) => {
+  res.status(400).send("Bad Request")
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
